09391ce9d059c0dabaf6cf39e0d1c49b31a81716
Christian Skubich <christian.skubich@googlemail.com>
2012-09-14 16:20:49 +0200

    Image will now be shown in master/detail layout.

--- src/de/skubware/opentraining/ExerciseDetailFragment.java
+++ src/de/skubware/opentraining/ExerciseDetailFragment.java
@@ -1,11 +1,13 @@
 package de.skubware.opentraining;
 
 import de.skubware.opentraining.basic.*;
+import de.skubware.opentraining.datamanagement.DataManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ExerciseDetailFragment extends Fragment {
@@ -30,7 +32,24 @@ public class ExerciseDetailFragment extends Fragment {
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_exercise_detail, container, false);
         if (exercise != null) {
-            ((TextView) rootView.findViewById(R.id.exercise_detail)).setText(exercise.getName());
+    		// Images
+    		ImageView imageview = (ImageView) (ImageView) rootView.findViewById(R.id.imageview);
+    		if (!exercise.getImagePaths().isEmpty()) {
+    			imageview.setImageDrawable(DataManager.INSTANCE.getDrawable(exercise.getImagePaths().get(0).toString()));
+    		} else {
+    			imageview.setImageResource(R.drawable.defaultimage);
+    		}
+        	
+        	// Image license
+    		TextView image_license = (TextView) rootView.findViewById(R.id.textview_image_license);
+    		if (exercise.getImageLicenseMap().values().iterator().hasNext()) {
+    			image_license.setText(exercise.getImageLicenseMap().values().iterator().next());
+    		} else {
+    			image_license.setText("Keine Lizenzinformationen vorhanden");
+    		}
+
+
+            //((TextView) rootView.findViewById(R.id.exercise_detail)).setText(exercise.getName());
         }
         return rootView;
     }
