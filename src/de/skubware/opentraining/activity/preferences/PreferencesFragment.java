f4133bd26c8d4b4fdfbb9e50949a23ec9683b19f
Christian Skubich <christian.skubich@googlemail.com>
2012-09-16 10:15:16 +0200

    Refactoring of PreferenceFragment.
    Code is now shorter.
    Found a bug at ExerciseDetailFragment, will be fixed with next commit.

--- src/de/skubware/opentraining/activity/preferences/PreferencesFragment.java
+++ src/de/skubware/opentraining/activity/preferences/PreferencesFragment.java
@@ -6,51 +6,42 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.util.Log;
 
+/**
+ * Fragment for equipment preferences.
+ */
 public class PreferencesFragment extends PreferenceFragment {
 
 	/** Tag for logging */
 	private static final String TAG = "PreferencesFragment";
 	
+	/** Empty constructor is required */
+	public PreferencesFragment(){ }
+
 	
-	public PreferencesFragment() {
-
-	}
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
         addPreferencesFromResource(R.xml.preferences_equipment);
         
+        // load settings
         SharedPreferences settings = this.getActivity().getSharedPreferences("OpenTraining", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = settings.edit();
-        
         
         for(SportsEquipment eq:SportsEquipment.values()){
             CheckBoxPreference pref = new CheckBoxPreference(this.getActivity());
             pref.setTitle(eq.toString());
             pref.setKey(eq.toString());
-            pref.setChecked(settings.getBoolean(eq.toString(), true));
-            pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener(){
-				@Override
-				public boolean onPreferenceChange(Preference pref, Object bo) {
-					SharedPreferences settings = PreferencesFragment.this.getActivity().getSharedPreferences("OpenTraining", Context.MODE_PRIVATE);
-			        SharedPreferences.Editor editor = settings.edit();
-			        editor.putBoolean(pref.getKey(), (Boolean) bo);
-					Log.i(TAG, pref.getKey() + " is set to " + bo.toString());
-					return true;
-				}
-            });
+            
+            boolean enabled = settings.getBoolean(eq.toString(), true);
+            pref.setChecked(enabled);
+            
             this.getPreferenceScreen().addPreference(pref);
-            editor.putBoolean(eq.toString(), true);
+            Log.i(TAG, eq.toString() + " was loaded, value is: " + enabled); 
         }
 
-        editor.commit();
 
         
 	}
