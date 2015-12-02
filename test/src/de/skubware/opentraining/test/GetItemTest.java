0829d8dd622cff58f33f82deab7c76ceb0b26ec7
Sam Kazerouni <samkazerouni@gmail.com>
2015-12-01 23:22:30 -0500

    Test Cases for Fragment Type
    GetItemTest.java contain the test cases for each Fragment type

---
+++ test/src/de/skubware/opentraining/test/GetItemTest.java
@@ -0,0 +1,45 @@
+package de.skubware.opentraining.test.RefactoringTest;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+import de.skubware.opentraining.activity.create_exercise.CreateExerciseActivity;
+
+/**
+ * Created by Joseph Daaboul and Sam Kazerouni on 12/1/2015.
+ */
+
+/**
+ * Tests for {@link CreateExerciseActivity}.
+ *
+ */
+public class GetItemTest extends AndroidTestCase {
+
+    public static final String TAG = "GetItemTest";
+    CreateExerciseActivity exerciseActivity = new CreateExerciseActivity();
+
+    public void TestNameFragment() {
+        NameFragment nameFragment = new NameFragment();
+        Assert.assertSame("They are not the same", nameFragment, exerciseActivity.mSectionsPagerAdapter.getItem(0));
+    }
+
+    public void TestDescriptionFragment() {
+        DescriptionFragment descFragment = new DescriptionFragment();
+        Assert.assertSame("They are not the same", descFragment, exerciseActivity.mSectionsPagerAdapter.getItem(1));
+    }
+
+    public void TestImageFragment() {
+        ImageFragment imageFragment = new ImageFragment();
+        Assert.assertSame("They are not the same", imageFragment, exerciseActivity.mSectionsPagerAdapter.getItem(2));
+    }
+
+    public void TestMuscleDataFragment() {
+        MuscleDataFragment muscleFragment = new MuscleDataFragment();
+        Assert.assertSame("They are not the same", muscleFragment, exerciseActivity.mSectionsPagerAdapter.getItem(3));
+    }
+
+    public void TestEquipmentDataFragment() {
+        EquipmentDataFragment equipmentFragment = new EquipmentDataFragment();
+        Assert.assertSame("They are not the same", equipmentFragment, exerciseActivity.mSectionsPagerAdapter.getItem(4));
+    }
+
+}
